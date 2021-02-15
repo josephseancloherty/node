68f0a8a6160ad9fb8a1da43618ec2b047d78c711
Joseph Cloherty <josephcloherty706@gmail.com>
2021-02-15 13:48:47 +0000

    13:48

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
-const express = require('express');
-const app = express();
-
-app.get('/', (req, res) => {
-    res.send('Welcome to the body of the home page')
-});
-
+const http = require('http')
+const fs = require('fs')
 const port = process.env.port || 3000;
-app.listen(port, () => {
-    console.log("WelcometoServer")
+
+const server = http.createServer(function(req, res) {
+    res.writeHead(200, { 'Centent-Type': 'text/html' })
+    fs.readFile('index.html', function(error, data) {
+        if (error){
+            res.writeHead(404)
+            res.write('error file not found')
+        } else {
+            res.write(data)
+        }
+        res.end()
+    })
+})
+
+server.listen(port, function(error) {
+if (error) {
+    console.log('Smething went Wrong', error)
+} else {
+    console.log('Server is listening on port ' + port)
+}
 })
