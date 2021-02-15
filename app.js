9a50f0b2b376e43d01131f78fc7078ed2d7b4670
Joseph Cloherty <josephcloherty706@gmail.com>
2021-02-15 10:47:25 +0000

    updated app.js file

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const port = process.env.port || 3000;
 
 app.get('/', (req, res) => {
     res.send('Welcome to the body of the home page')
 });
 
+const port = process.env.port || 3000;
 app.listen(port, () => {
-    console.log("Ello old Chap")
+    console.log("WelcometoServer")
 })
